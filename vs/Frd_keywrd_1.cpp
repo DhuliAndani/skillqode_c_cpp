e63058e870b6e458a307947c9ea49b64d5a66915
dhuli <dhruviandani0601@gmail.com>
2022-10-12 11:28:27 +0530

    First commit

---
+++ vs/Frd_keywrd_1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class Box
+{
+private:
+    int length;
+public:
+    Box() : length(0)
+    {
+
+    }
+    friend int printlength(Box);    
+};
+int printlength(Box b)
+{
+    b.length +=10;
+    return b.length;
+}
+int main()
+{
+    Box b;
+    cout<<"Length Of Box : "<<printlength(b)<<endl;
+    return 0;
+}
