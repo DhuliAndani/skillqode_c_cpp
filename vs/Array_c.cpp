46632bbd577c5edc3c17c8be07050e77584af389
dhuli <dhruviandani0601@gmail.com>
2022-11-17 11:37:40 +0530

    First commit

--- vs/Array_c.cpp
+++ vs/Array_c.cpp
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    float a[]={11.4,22,33,44,55};
+    int a[]={11,22,33,44,55};
     for(int i=0;i<5;i++)
     {
-        printf("\n %.2f - %u",a[i],&a[i]);
+        printf("\n %d-%u",a[i],&a[i]);
     }
-    printf("\n %f",sizeof(float));
+    printf("\n %d",sizeof(int));
 }
